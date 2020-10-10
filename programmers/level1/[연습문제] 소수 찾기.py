3a3823ea90f241eaa1f765e8dbf8b4efad8fe978
leeyongjoo <2yongjooo@gmail.com>
2020-10-11 03:01:31 +0900

    [연습문제] 소수 찾기: 코드 수정

--- programmers/level1/[연습문제] 소수 찾기.py
+++ programmers/level1/[연습문제] 소수 찾기.py
@@ -3,15 +3,17 @@ def solution(n):
     # 에라토스테네스의 체
     prime_arr = [False, False] + [True] * (n - 1)
     for i in range(2, n + 1):
-        for j in range(i * 2, n + 1, i):
-            prime_arr[j] = False
+        if prime_arr[i] is True:
+            for j in range(i * 2, n + 1, i):
+                prime_arr[j] = False
     return len([_ for _ in prime_arr if _])
 
 
 # def solution(n):
 #     nums = set(range(2, n + 1))
 #     for i in list(nums):
-#         nums -= set(range(2 * i, n + 1, i))
+#         if i in nums:
+#             nums -= set(range(2 * i, n + 1, i))
 #     return len(nums)
 
 
